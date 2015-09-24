0094c2315d98095ab52f7e50e3e1524185fc622f
Luan Pham <luanpm88@gmail.com>
2015-09-24 22:49:59 +0700

    update

--- app/controllers/payment_records_controller.rb
+++ app/controllers/payment_records_controller.rb
@@ -253,7 +253,7 @@ class PaymentRecordsController < ApplicationController
         end
       end
       
-      @list = @list.sort! { |a,b| a[:contact_name]+a[:course] <=> b[:contact_name]+b[:course] }
+      @list = @list.sort! { |a| a[:contact_name]+a[:course].to_s }
       
       @papers = Subject.where(id: paper_ids).order("name")
       
