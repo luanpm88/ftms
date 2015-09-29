023ace9867be6d6b111b4cab96f701629e8bd29f
Luan Pham <luanpm88@gmail.com>
2015-09-29 15:57:21 +0700

    fix bug

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1453,7 +1453,7 @@ class Contact < ActiveRecord::Base
           remove_course = false
           
           
-          if row[:course].id == transfer.course.id && row[:created_at] < transfer.created_at
+          if !row[:course].nil? && row[:course].id == transfer.course.id && row[:created_at] < transfer.created_at
             
             # upfront course
             if transfer.course.upfront == true
