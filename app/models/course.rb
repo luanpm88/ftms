68f26ea17e54c675d3af208b31432f8df8443c6a
Luan Pham <luanpm88@gmail.com>
2015-09-30 14:16:38 +0700

    fix bug

--- app/models/course.rb
+++ app/models/course.rb
@@ -228,7 +228,7 @@ class Course < ActiveRecord::Base
     @student = Contact.find(params[:students])
     
     @courses = self.filters(params, user)
-    @course_ids = @courses.map(&:id)
+    @course_ids = @courses.includes(:contacts).map(&:id)
     
     @all_courses =  @student.active_courses_with_phrases
     
