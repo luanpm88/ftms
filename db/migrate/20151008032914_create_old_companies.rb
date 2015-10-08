d95450017ca9c6b3a5634033b5925297601ba6d2
Luan Pham <luanpm88@gmail.com>
2015-10-07 20:35:01 -0700

    fix bug

---
+++ db/migrate/20151008032914_create_old_companies.rb
@@ -0,0 +1,16 @@
+class CreateOldCompanies < ActiveRecord::Migration
+  def change
+    create_table :old_companies do |t|
+      t.text :company_id 
+      t.text :company_name 
+      t.text :company_address 
+      t.text :company_manager 
+      t.text :company_off_phone 
+      t.text :company_fax 
+      t.text :company_manager_hphone 
+      t.text :company_email
+      
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20151008033152_create_old_deliveries.rb
@@ -0,0 +1,14 @@
+class CreateOldDeliveries < ActiveRecord::Migration
+  def change
+    create_table :old_deliveries do |t|
+      t.text :delivery_id 
+      t.text :student_id 
+      t.text :subject_id 
+      t.text :book_type 
+      t.text :book_vol 
+      t.text :delivery_yes
+
+      t.timestamps null: false
+    end
+  end
+end
