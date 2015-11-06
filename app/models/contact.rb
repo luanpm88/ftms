88a67228f32b2301a3fa89b77c9f95829eeaf6ba
Luan Pham <luanpm88@gmail.com>
2015-11-06 05:52:31 -0800

    update 06-11-2015

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1508,13 +1508,10 @@ class Contact < ActiveRecord::Base
       drafts = drafts.select{|c| c.contact_types.order("name").map(&:name).join("") != self.contact_types.order("name").map(&:name).join("")}
     else
       value = value.nil? ? self[type] : value
-      drafts = drafts.where("#{type} IS NOT NUll AND #{type} != ?", value)
+      drafts = drafts.where("#{type} IS NOT NUll AND (#{type} != ? OR contacts.id = #{self.current.id})", value)
     end    
-    aa = [self.current]
-    drafts.each do |i|
-      aa << i
-    end
-    return aa
+    
+    return (drafts.count > 1) ? drafts : []
   end
   
   def self.status_options
