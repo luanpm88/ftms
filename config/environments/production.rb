1429f8421b7e7ce8fcc3ab982dea0a6ee4509a21
Luan Pham <luanpm88@gmail.com>
2015-11-16 18:02:37 -0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Hkerp::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "luanpm@hoangkhang.com.vn",
-    :password             => "ngahoang",
+    :user_name            => "soft.support@hoangkhang.com.vn",
+    :password             => "aA456321@",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
