dcadf3c4c9b384da13851e8b343ad5f44fe5d28f
Luan Pham <luanpm88@gmail.com>
2015-11-29 17:06:55 -0800

    course register history

--- app/helpers/course_registers_helper.rb
+++ app/helpers/course_registers_helper.rb
@@ -1,7 +1,7 @@
 module CourseRegistersHelper
   def render_course_register_actions(item, size=nil)
     size = size.nil? ? "mini" : size
-    actions = '<div class="text-right"><div class="btn-group actions">
+    actions = '<div class="text-right but"><div class="btn-group actions">
                     <button class="btn btn-'+size+' btn-white btn-demo-space dropdown-toggle" data-toggle="dropdown">Actions <span class="caret"></span></button>'
       actions += '<ul class="dropdown-menu">'
       
@@ -60,7 +60,7 @@ module CourseRegistersHelper
       actions += '<li class="divider"></li>' if group_3 > 0
       
       if can? :read, item
-        actions += '<li>'+ActionController::Base.helpers.link_to('View Detail', {controller: "course_registers", action: "show", id: item.id, tab_page: 1}, title: "Course Register Detail: #{item.contact.display_name}", class: "tab_page")+'</li>'
+        actions += '<li>'+ActionController::Base.helpers.link_to('View Detail', {controller: "course_registers", action: "show", id: item.id, tab_page: 1}, title: "Course/Stock Registration: #{item.contact.display_name}", class: "tab_page")+'</li>'
       end
       
       if can? :read, item
@@ -81,6 +81,22 @@ module CourseRegistersHelper
       #end
       
       
+      
+      actions += '</ul></div></div>'
+      
+      return actions.html_safe
+  end
+  
+  def render_course_register_history_actions(item)
+    return "" if item.revisions.empty?
+    
+      actions = '<div class="text-right but"><div class="btn-group actions">
+                    <button class="btn btn-big btn-white btn-demo-space dropdown-toggle" data-toggle="dropdown">Histories <span class="caret"></span></button>'
+      actions += '<ul class="dropdown-menu">'
+      
+      item.revisions.order("created_at DESC").each do |d|
+        actions += '<li>'+ActionController::Base.helpers.link_to("#{d.created_at.strftime("%d-%b-%Y %I:%M %p")}", {controller: "course_registers", action: "show", id: d.id, tab_page: 1}, title: "[#{d.created_at.strftime("%d-%b-%Y %I:%M %p")}] Course/Stock Registration: #{item.contact.display_name}", class: "tab_page")+'</li>'        
+      end
       
       actions += '</ul></div></div>'
       
